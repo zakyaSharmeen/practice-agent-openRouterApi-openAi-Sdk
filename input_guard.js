cafa7cde9487da2c8e64e723540d1411421f37ab
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-03-13 19:26:16 +0530

    input-guardrails

--- input_guard.js
+++ input_guard.js
@@ -1,6 +1,34 @@
 process.env.OPENAI_AGENTS_DISABLE_TRACING = "true";
 import "dotenv/config";
-import { Agent, run, tool } from "@openai/agents";
+import { Agent, run } from "@openai/agents";
+import { z } from "zod";
+
+//agent
+const mathInputAgent = new Agent({
+  name: "Math Agent",
+  instructions:
+    "you are an input guardrail agent for math homework, you will check if the user input is related to math homework or not, if it is related to math homework then you will allow the input to pass through and",
+  model: "openai/gpt-4o",
+  outputType: z.object({
+    isValidMathQuestion: z
+      .boolean()
+      .describe("Indicates if the input is a valid math question."),
+  }),
+});
+
+//watchman carring tripwire triggered or not
+const mathInputGuardrails = {
+  name: "Math Homework Guardrails",
+  execute: async ({ input }) => {
+    console.log("INPUT GUARDRAILS CHECKED::::::::::::::", input);
+    const result = await run(mathInputAgent, input);
+    console.log("INPUT GUARDRAILS RESULT::::::::::::::", result.finalOutput);
+
+    return {
+      tripwireTriggered: result.finalOutput.isValidMathQuestion ? false : true,
+    };
+  },
+};
 
 //agent
 const mathAgent = new Agent({
@@ -8,8 +36,10 @@ const mathAgent = new Agent({
   instructions:
     "You are a helpful assistant that can perform basic math calculations. give answer to the point",
   model: "openai/gpt-4o",
+  inputGuardrails: [mathInputGuardrails],
 });
 
+///user-question
 async function main(query = "") {
   const result = await run(mathAgent, query);
   console.log(
@@ -18,4 +48,8 @@ async function main(query = "") {
   );
 }
 // main("What is 2 + 2 *8/4?");
-main("2^3");
+main("write a js code to calcute sum of 2 numbers");
+main("write a js code to calcute sum of 2 numbers");
+
+///see here if u asking it to write code it will write the code and then execute the code and give you the answer, it is not just writing the code but also executing it and giving you the answer.
+// and now we will put limitation to it using -guardrails
